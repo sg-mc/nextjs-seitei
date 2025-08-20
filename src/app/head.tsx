02079c423d935b9f9977f69a55ef7f20b37b0c31
sg-mc <d.tanaka@saintgrail.org>
2025-08-20 16:08:24 +0900

    tag

---
+++ src/app/head.tsx
@@ -0,0 +1,23 @@
+export default function Head() {
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-ZV3NSZHERG"
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZV3NSZHERG');
+`,
+        }}
+      />
+    </>
+  );
+}
+
