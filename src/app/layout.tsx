579db2bbd18d0fa34bc41bfe80a4adfc73b2625a
sg-mc <d.tanaka@saintgrail.org>
2025-08-20 16:12:18 +0900

    tag2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZV3NSZHERG"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-ZV3NSZHERG');
+`,
+          }}
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
